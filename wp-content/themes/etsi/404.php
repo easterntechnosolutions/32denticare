f4107258cbc101d56eac447b110e073ecd309cf9
jagrutiparihar-easternts <115872045+jagrutiparihar-easternts@users.noreply.github.com>
2024-08-29 15:21:00 +0530

    404 page changes

--- wp-content/themes/etsi/404.php
+++ wp-content/themes/etsi/404.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?>
 
-       <section class="not-found-page">
-          <div class="container">
-            <div class="mainhead">
-                <h1>404</h1>
-            </div>
-            <div class="sub-head">
-                <h2>
-                    Oops! This page can’t be found anywhere.
-                </h2>
-              <p>  The page you are looking for doesn’t exist or has been moved. </p>
-            </div>
-        <a href="https://bestdentistinvesusurat.easyglue.in/">   <button class="back-home-btn"> Back To Home </button> </a>
-          </div>
-        </section><!--main-->
+<section class="not-found-page">
+  <div class="container">
+    <div class="mainhead">
+        <h1>404</h1>
+    </div>
+    <div class="sub-head">
+        <h2> Oops! This page can’t be found anywhere.</h2>
+        <p> The page you are looking for doesn’t exist or has been moved. </p>
+    </div>
+    <a href="<?php echo site_url(); ?>"><button class="back-home-btn"> Back To Home </button></a>
+  </div>
+</section><!--main-->
 
 <?php get_footer(); ?>
